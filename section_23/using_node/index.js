0ffce4ba58291ef3b2e1630e254aff65e9122086
ammar-alsuhaimi <152860551+ammar-alsuhaimi@users.noreply.github.com>
2025-02-19 14:55:11 +0300

    upgrade the project to make it visable

--- section_23/using_node/index.js
+++ section_23/using_node/index.js
@@ -1,1 +1,14 @@
-console.log('hello from Node!')
+//console.log('hello from Node!')
+
+
+import express from 'express'; // Use ES module syntax for import
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Node!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
