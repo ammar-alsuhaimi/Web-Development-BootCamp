c95e1ac348e1f869d916b5c4b369dcd866d43e6a
ammar-alsuhaimi <152860551+ammar-alsuhaimi@users.noreply.github.com>
2025-02-19 14:20:38 +0300

    edit the code to make it ineractive with the user

--- section_23/qr_code_project/index.js
+++ section_23/qr_code_project/index.js
@@ -1,52 +1,10 @@
-
-/* THIS IS THE PROGRAM THAT HAVE FROM THE COURSE.
-
-1. Use the inquirer npm package to get user input.
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
-
-/*
-import inquirer from 'inquirer';
-import qr from 'qr-image';
-import fs from 'fs'
-inquirer
-  .prompt([
-    {
-        message: "Type in your URL: ",
-        name: "URL"
-    }
-
-  ])
-  .then((answers) => {
-    const url=answers.URL;
-    var qr_svg = qr.image(url);
-    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
-    fs.writeFile("URL.txt", url, (err) => {
-          if (err) throw err;
-          console.log("The file has been saved!");
-        });
-})
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
-  */
-
-
-// THIS IS THE PROGRAM THAT I HAVE CREATED. TO MAKE IT WORK BETTER TO INTERACTIVE WITH THE USER.
-
 import express from 'express';
-import fs from 'fs';
 import qr from 'qr-image';
-import inquirer from 'inquirer';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
@@ -61,13 +19,18 @@ app.get('/', (req, res) => {
 
 app.post('/generate', (req, res) => {
   const url = req.body.url;
-  const qr_svg = qr.image(url);
-  qr_svg.pipe(fs.createWriteStream('qr_img.png'));
 
-  fs.writeFile('URL.txt', url, (err) => {
-    if (err) throw err;
-    res.send('The QR code and URL file have been saved!');
-  });
+  if (!url) {
+    return res.status(400).send('URL is required');
+  }
+
+  try {
+    const qr_svg = qr.image(url, { type: 'svg' });
+    res.type('svg');
+    qr_svg.pipe(res);
+  } catch (error) {
+    res.status(500).send('Error generating QR code');
+  }
 });
 
 app.listen(PORT, () => {
