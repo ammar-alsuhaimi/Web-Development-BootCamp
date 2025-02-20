25ead5864352ec1e6d160b3c389a055d625c1e54
ammar-alsuhaimi <152860551+ammar-alsuhaimi@users.noreply.github.com>
2025-02-20 12:41:29 +0300

    changes content

--- section_24/express_server/index.js
+++ section_24/express_server/index.js
@@ -2,6 +2,14 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+// Serve static files from the 'public' directory
+app.use(express.static('public'));
+
+// Default route
+app.get('/', (req, res) => {
+  res.send('Hello from express server');
+});
+
 app.listen(port, () => {
   console.log(`Server running on port ${port}.`);
 });
